81c8e9b85330ca7f8c04687c6e663f57223d1e8d
Hassan Raza <hassan,raza@rev9solutions.com>
2024-12-08 17:46:25 +0500

    env

--- src/app/Interviews/view/[id]/page.jsx
+++ src/app/Interviews/view/[id]/page.jsx
@@ -30,35 +30,35 @@ function InterviewView({ params }) {
 
   if (loading) {
     return (
-      <div className="min-h-screen !bg-slate-900 flex justify-center items-center">
-        <div className="animate-spin rounded-full h-16 w-16 border-t-2 border-b-2 !border-blue-500"></div>
+      <div className="min-h-screen bg-slate-900 flex justify-center items-center">
+        <div className="animate-spin rounded-full h-16 w-16 border-t-2 border-b-2 border-blue-500"></div>
       </div>
     );
   }
 
   return (
-    <div className="min-h-screen !bg-slate-900">
-      <Navbar background={"!bg-[#0F172A] !text-white"} />
+    <div className="min-h-screen bg-slate-900">
+      <Navbar background={"bg-[#0F172A] text-white"} />
       <div className="max-w-full py-4 px-10">
         <Card
           className="overflow-hidden !bg-slate-800 !border !border-slate-700"
           title={
-            <h1 className="text-3xl font-bold !bg-gradient-to-r !from-blue-500 !to-purple-500 !text-transparent !bg-clip-text">
+            <h1 className="text-3xl font-bold bg-gradient-to-r from-blue-500 to-purple-500 text-transparent bg-clip-text">
               {interview?.title}
             </h1>
           }
         >
-          <div className="prose prose-invert max-w-none !text-white">
+          <div className="prose prose-invert  max-w-none text-white">
             <Markdown
               remarkPlugins={[remarkGfm]}
               components={{
                 table: ({ children }) => (
                   <table
                     style={{
-                      marginBottom: "20px !important",
-                      border: "1px solid black !important",
-                      borderRadius: "5px !important",
-                      margin: "auto !important",
+                      marginBottom: "20px",
+                      border: "1px solid black",
+                      borderRadius: "5px",
+                      margin: "auto",
                     }}
                   >
                     {children}
@@ -67,14 +67,14 @@ function InterviewView({ params }) {
                 th: ({ children }) => (
                   <th
                     style={{
-                      border: "1px solid #000 !important",
-                      paddingTop: "7px !important",
-                      paddingBottom: "7px !important",
-                      paddingRight: "10px !important",
-                      paddingLeft: "10px !important",
-                      backgroundColor: "#4D5E80 !important",
-                      color: "white !important",
-                      textAlign: "center !important",
+                      border: "1px solid #000",
+                      paddingTop: "7px",
+                      paddingBottom: "7px",
+                      paddingRight: "10px",
+                      paddingLeft: "10px",
+                      backgroundColor: "#4D5E80",
+                      color: "white",
+                      textAlign: "center",
                     }}
                   >
                     {children}
@@ -83,11 +83,11 @@ function InterviewView({ params }) {
                 td: ({ children }) => (
                   <td
                     style={{
-                      border: "1px solid #000 !important",
-                      paddingTop: "5px !important",
-                      paddingBottom: "5px !important",
-                      paddingRight: "7px !important",
-                      paddingLeft: "7px !important",
+                      border: "1px solid #000",
+                      paddingTop: "5px",
+                      paddingBottom: "5px",
+                      paddingRight: "7px",
+                      paddingLeft: "7px",
                     }}
                   >
                     {children}
